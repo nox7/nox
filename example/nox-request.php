496a4e70f26833d061a16c4a8ac998a71235df0e
Garet C. Green <17110935+nox7@users.noreply.github.com>
2021-08-18 11:13:55 -0500

    Update nox-request.php

--- example/nox-request.php
+++ example/nox-request.php
@@ -3,6 +3,7 @@
 	use Nox\ORM\Abyss;
 
 	require_once __DIR__ . "/../vendor/autoload.php";
+	require_once __DIR__ . "/nox-env.php";
 
 	$requestPath = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 	$requestMethod = $_SERVER['REQUEST_METHOD'];
@@ -17,6 +18,7 @@
 	);
 
 	// Load the Abyss ORM configuration
+	// Comment this out to disable using Abyss and require a MySQL connection
 	Abyss::loadConfig(__DIR__);
 
 	// Load the request handler

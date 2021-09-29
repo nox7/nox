493ea9f2e4792cb112732ae58232d7c53995ae87
Cole Green <17110935+nox7@users.noreply.github.com>
2021-09-29 09:34:12 -0500

    Add ModelClass::queryOne()

--- src/ORM/ModelClass.php
+++ src/ORM/ModelClass.php
@@ -39,6 +39,32 @@
 		}
 
 		/**
+		 * Performs a query on the instances of the ModelClass and returns a single matching ModelClass
+		 * that meets the criteria provided, or null.
+		 */
+		public static function queryOne(
+			ColumnQuery $columnQuery = null,
+			ResultOrder $resultOrder = null,
+			Pager $pager = null,
+		): ?ModelClass {
+			$abyss = new Abyss();
+
+			/** @var ModelClass[] $modelClasses */
+			$modelClasses = $abyss->fetchInstances(
+				model: static::getModel(),
+				columnQuery: $columnQuery,
+				resultOrder: $resultOrder,
+				pager: $pager,
+			);
+
+			if (empty($modelClasses)){
+				return null;
+			}else{
+				return $modelClasses[0];
+			}
+		}
+
+		/**
 		 * Utilizes the MySQL COUNT() function to quickly
 		 * fetch the number of results returned on this instance
 		 * and provided $columnQuery

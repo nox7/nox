744dcc277ecf35587b990c97a1824d548b1ed4a5
Cole Green <17110935+nox7@users.noreply.github.com>
2022-04-17 07:27:43 -0500

    Updates the example directory to modern Nox standards

--- example/controllers/ExampleAPIController.php
+++ example/controllers/ExampleAPIController.php
@@ -3,10 +3,16 @@
 	// No need to require the autoload in the Controller.
 	// The controller has the scope of the request.php file
 
+	use Nox\Http\Attributes\ProcessRequestBody;
+	use Nox\Http\Attributes\UseJSON;
+	use Nox\Http\JSON\JSONResult;
+	use Nox\Http\JSON\JSONSuccess;
+	use Nox\Router\Attributes\Controller;
 	use Nox\Router\Attributes\Route;
 	use Nox\Router\Attributes\RouteBase;
 	use Nox\Router\BaseController;
 
+	#[Controller]
 	#[RouteBase("/\/api\/(?<version>v\d)/", true)]
 	class ExampleAPIController extends BaseController{
 
@@ -15,8 +21,17 @@
 			return "API home view example.";
 		}
 
-		#[Route("GET", "/sub-page")]
-		public function subView(): string{
-			return "API sub page example";
+		#[Route("PUT", "/some/resource/to/put")]
+		#[UseJSON] // Lets the router know to put the response content-type as JSON and to send JSONResult as a JSON string
+		#[ProcessRequestBody] // Parses the raw request body if it is a non-GET request
+		public function subView(): JSONResult{
+			// Payload is the request body parsed into an array
+			$payload = BaseController::$requestPayload;
+
+			if (isset($payload['some-data'])){
+				// Insert into DB or something
+			}
+
+			return new JSONSuccess();
 		}
 	}
--- example/controllers/HomeController.php
+++ example/controllers/HomeController.php
@@ -5,21 +5,41 @@
 
 	require_once __DIR__ . "/../attributes/NeverUsable.php";
 
+	use Nox\RenderEngine\Exceptions\LayoutDoesNotExist;
+	use Nox\RenderEngine\Exceptions\ViewFileDoesNotExist;
 	use Nox\RenderEngine\Renderer;
+	use Nox\Router\Attributes\Controller;
 	use Nox\Router\Attributes\Route;
+	use Nox\RenderEngine\Exceptions\ParseError;
 
+	#[Controller]
 	class HomeController extends \Nox\Router\BaseController{
 
+		/**
+		 * @throws ParseError
+		 * @throws ViewFileDoesNotExist
+		 * @throws LayoutDoesNotExist
+		 */
 		#[Route("GET", "/")]
 		public function homeView(): string{
 			return Renderer::renderView("home.html");
 		}
 
+		/**
+		 * @throws ParseError
+		 * @throws ViewFileDoesNotExist
+		 * @throws LayoutDoesNotExist
+		 */
 		#[Route("GET", "/404")]
 		public function error404View(): string{
 			return Renderer::renderView("errors/404.html");
 		}
 
+		/**
+		 * @throws ParseError
+		 * @throws ViewFileDoesNotExist
+		 * @throws LayoutDoesNotExist
+		 */
 		#[Route("GET", "/always-404")]
 		#[NeverUsable()]
 		public function always404View(): string{

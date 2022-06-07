57499093ba14473dc6cb66761a5556a24eb90485
Garet C. Green <17110935+nox7@users.noreply.github.com>
2022-06-07 09:39:28 -0500

    Add 'byFunction' for ResultOrder
    Also change return types to 'self'

--- src/ORM/ResultOrder.php
+++ src/ORM/ResultOrder.php
@@ -6,14 +6,21 @@
 
 		public array $orderClauses = [];
 
-		public function __construct(){
-
+		/**
+		 * Inputs a result order but using function syntax
+		 * E.g. "RAND()"
+		 * @param string $functionCallString
+		 * @return $this
+		 */
+		public function byFunction(string $functionCallString): self{
+			$this->orderClauses[] = $functionCallString;
+			return $this;
 		}
 
 		/**
 		 * Adds a column order clause. Returns itself for chaining.
 		 */
-		public function by(string $columnName, string $order): ResultOrder{
+		public function by(string $columnName, string $order): self{
 			$this->orderClauses[] = sprintf("`%s` %s", $columnName, $order);
 			return $this;
 		}
